900de164cfefe0ad4293e548446dadad1282e907
Natik22 <nicolas.2205@hotmail.fr>
2024-03-13 11:51:34 +0100

    save nos produits

---
+++ nosproduits.php
@@ -0,0 +1,31 @@
+<?php require_once __DIR__ . '/components/head.php';
+require_once __DIR__ . '/components/header.php';
+ ?>
+
+
+<div class="container-fluid text-center g-0">
+  <div class="cols d-flex justify-content-evenly p-5 g-0">
+    <div class="card col-12 col-sm-3 col-md-3 col-lg-3 col-xl-3">
+
+      <div class="align-item-center">
+        <img src="..." class="img-thumbnail" alt="pochette produit">
+      </div>
+
+      <div class="card-body">
+        <h5 class="card-title">Le titre de l'article</h5>
+        <p class="card-text"></p>
+      </div>
+
+      <ul class="list-group list-group-flush">
+        <li class="list-group-item"></li>
+        <li class="list-group-item"></li>
+      </ul>
+
+    </div>
+  </div>
+</div>
+
+
+
+<?php require_once __DIR__ . '/components/footer.php';?>
+
