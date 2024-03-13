507b631369cd234cfb6eeb6cb96ac1b71df78400
Taki <151673628+ItakiI@users.noreply.github.com>
2024-03-13 12:38:19 +0100

    header modif 2

--- components/header.php
+++ components/header.php
@@ -1,32 +1,46 @@
-<header>
-    <nav class="navbar navbar-expand-lg bg-body-tertiary bg-success p-0 ">
-        <div class="container-fluid d-flex justify-content-between bg-success">
+<!DOCTYPE html>
+<html lang="fr">
 
-            <a class="navbar-brand d-flex align-items-center" href="/medicsong/views/admin.php"><img src="/medicsong/assets/img/vinnylmedico.gif" alt="Medic'Song" width="120" height="70">
-                <h1>Medicsong</h1>
-            </a>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Medicsong</title>
+    <link rel="stylesheet" href="/medicsong/assets/style/style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <script defer src="https://kit.fontawesome.com/f907ded947.js" crossorigin="anonymous"></script>
+    <script defer src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</head>
 
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse justify-content-end pe-5" id="navbarNavDropdown">
-                <ul class="navbar-nav">
+<body class="bg-warning-subtle">
+    <header>
+        <nav class="navbar navbar-expand-lg bg-body-tertiary bg-success p-0 ">
+            <div class="container-fluid d-flex justify-content-between bg-success">
+                <div class=" d-flex align-items-center flex-wrap">
+                <a class="navbar-brand" href="#"><img src="/medicsong/assets/img/vinnylmedico.gif" alt="Medic'Song" width="120" height="70">     
+                </a>
+                <a><h1>Medicsong</h1></a>
+                </div>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse justify-content-end pe-5" id="navbarNavDropdown">
+                    <ul class="navbar-nav">
 
-                    </li>
-                    <li class="nav-item me-5">
-                        <a class="nav-link active" href="/medicsong/index.html.php">ACCEUIL</a>
-                    </li>
-                    <li class="nav-item me-5">
-                        <a class="nav-link active" href="/medicsong/views/admin.php">ADMINISTRATION</a>
-                    </li>
-                    <li class="nav-item me-5">
-                        <a class="nav-link active" href="#">NOS PRODUIT</a>
-                    </li>
-                    <li class="nav-item me-5">
-                        <a class="nav-link active" href="/medicsong/views/contact.html.php">CONTACT</a>
-                    </li>
-                </ul>
+                        </li>
+                        <li class="nav-item me-4">
+                            <a class="nav-link active" href="/medicsong/index.html.php">ACCEUIL</a>
+                        </li>
+                        <li class="nav-item me-4">
+                            <a class="nav-link active" href="/medicsong/views/admin.php">ADMINISTRATION</a>
+                        </li>
+                        <li class="nav-item me-4">
+                            <a class="nav-link active" href="#">NOS PRODUIT</a>
+                        </li>
+                        <li class="nav-item me-4">
+                            <a class="nav-link active" href="/medicsong/views/contact.html.php">CONTACT</a>
+                        </li>
+                    </ul>
+                </div>
             </div>
-        </div>
-    </nav>
-</header>
+        </nav>
+    </header>
