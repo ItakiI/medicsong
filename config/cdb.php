80333bca0fc21b542589ba0148d8dccb80843974
Yoann-SCELLIER <scellier.yoann@gmail.com>
2024-03-14 16:34:30 +0100

    modification formulaire : produit / ajout perso / ajout spec + légère modif de la banniere contact

--- config/cdb.php
+++ config/cdb.php
@@ -4,10 +4,16 @@
 
 try {
     // On se connecte à MySQL
-    $bdd = new PDO('mysql:host=localhost;dbname=medicsong', 'root', 'Nico2205!');
+    $bdd = new PDO('mysql:host=localhost;dbname=medicsong', 'yoann', 'Simplon2023!');
 } catch (Exception $e) {
     // En cas d'erreur, on affiche un message et on arrête tout
 
     die('Erreur : ' . $e->getMessage());
 }
 // Si tout va bien, on peut continuer
+
+
+// Ajouter vos ID pour vous connectez en commentaire pour faire du copier coller pour changer lors des pulls
+// Yoann = $bdd = new PDO('mysql:host=localhost;dbname=medicsong', 'yoann', 'Simplon2023!');
+// Taki =
+// Nico =
