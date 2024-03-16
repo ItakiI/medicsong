11c9cc2769615171b18472143d01119ff6265ec9
Natik22 <nicolas.2205@hotmail.fr>
2024-03-16 16:02:30 +0100

    réorganisation des fichiers + rangement

---
+++ controller/add_produits.php
@@ -0,0 +1,9 @@
+<?php
+require_once dirname(__DIR__) . '/config/cdb.php';
+require_once dirname(__DIR__) . '/function/produits.fn.php';
+
+addProduits($bdd);
+
+header('Location: /medicsong/views/admin.php'); 
+
+?>
