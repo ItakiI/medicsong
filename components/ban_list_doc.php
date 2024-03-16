acd83e88b726654cb90fba369475ceaf6f66f533
Yoann-SCELLIER <scellier.yoann@gmail.com>
2024-03-16 14:33:18 +0100

    rajout image doc + mp3

--- components/ban_list_doc.php
+++ components/ban_list_doc.php
@@ -2,7 +2,7 @@
     <img src="/medicsong/assets/img/image1.webp" class="w-auto" alt="...">
     <div class="card-img-overlay">
         <div class="d-flex justify-content-end m-3 b-3 p-3 fs-3">
-            <p class="slogan">“Elevate to new heights: Music that uplifts your well-being.”</p>
+            <p class="slogan">“The doctors of hope: Healing with confidence, enlightening with compassion.”</p>
         </div>
     </div>
 </div>
