a7825b7f257f22503fc08763b4960290f016d2aa
Yoann-SCELLIER <scellier.yoann@gmail.com>
2024-03-17 21:37:52 +0100

    rajout de photo pour album

--- card_produits.php
+++ card_produits.php
@@ -25,31 +25,32 @@ require_once __DIR__ . '/components/ban_produit.php';
       // affichage des produits
   ?>
 
-      <div>
-        <div class="card mb-3 m-3" style="max-height: 560px; width: auto">
-          <div class="row g-0">
-            <div class="col-md-5 d-flex align-self-center">
-              <img src="<?php echo $produit["image"] ?>" class=" img-fluid rounded-start h-100 w-100 m-3 border-bottom-0 border border-black" alt="">
-            </div>
-            <div class="col-md-7">
-              <div class="card-body m-0 mb-0 b-0 bb-0 p-0 pb-0 g-0 gb-0">
-                <h5 class="card-title text-center"><?php echo $produit["nom"] ?></h5>
-                <p>Prix : <?php echo $produit["prix"] ?><br>
-                  Stock : <?php echo $produit["stock"] ?><br>
-                  Vendu : <?php echo $produit["vendu"] ?><br>
-                  Titre : <?php echo $produit["titre"] ?><br>
-                  Titre : <?php echo $produit["titre_1"] ?><br>
-                  Titre : <?php echo $produit["titre_2"] ?></p>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div class="card-body d-flex justify-content-evenly mb-3">
-          <a href="/medicsong/views/formmodifproduits.php?id=<?php echo $produit['id'] ?>" class="card-link btn btn-success">Modifier</a>
-          <a href="/medicsong/controller/delete_produits.php?id=<?php echo $produit['id'] ?>" class="card-link btn btn-danger">Supprimer</a>
+<div>
+  <div class="card border border-black bg-info-subtle mb-3 m-3" style="max-height: 560px; width: auto">
+    <div class="row g-0">
+      <div class="col-md-7 d-flex align-self-center">
+        <img src="<?php echo $produit["image"] ?>" class=" img-fluid rounded-start h-100 w-100 m-3 border-bottom-0 border border-black" alt="">
+      </div>
+      <div class="col-md-5 align-self-center">
+        <div class="card-body">
+          <h5 class="card-title text-center fs-6"><?php echo $produit["nom"] ?></h5>
+          <p class="fs-6">Prix : <?php echo $produit["prix"] ?>€<br>
+          Stock : <?php echo $produit["stock"] ?><br>
+          Vendu : <?php echo $produit["vendu"] ?><br>
+          Titre : <br><?php echo $produit["titre"] ?><br>
+          Titre : <br><?php echo $produit["titre_1"] ?><br>
+          Titre : <br><?php echo $produit["titre_2"] ?></p>
         </div>
       </div>
-  <?php
+    </div>
+  </div>
+  <div class="card-title d-flex justify-content-evenly mb-3">
+    <a href="/medicsong/views/formmodifproduits.php?id=<?php echo $produit['id'] ?>" class="card-link border border-black btn btn-success d-grid gap-3 col-4 mx-auto">Modifier</a>
+    <a href="/medicsong/controller/delete_produits.php?id=<?php echo $produit['id'] ?>" class="card-link border border-black btn btn-danger d-grid gap-3 col-4 mx-auto">Supprimer</a>
+  </div>
+</div>
+
+<?php
 
     }
   } else {
--- views/card_docteur.php
+++ views/card_docteur.php
@@ -1,4 +1,6 @@
-<div class="d-flex justify-content-evenly row row-cols-sm-2 row-cols-md-3 row-cols-lg-3 row-cols-xl-4 m-3 b-3 p-3 g-0">
+<div class="d-flex justify-content-evenly row row-cols-sm-2 row-cols-md-3 row-cols-lg-4 row-cols-xl-6 m-3 b-3 p-3 g-0">
+<!-- <div class="d-flex justify-content-evenly row row-cols-sm-1 row-cols-md-2 row-cols-lg-3 row-cols-xl-3 m-3 b-3 p-3 g-0"> -->
+
 
   <?php
   foreach ($result as $row) {
@@ -7,9 +9,9 @@
   ?>
 
 
-    <div class="card text-center border border-black m-3 bg-secondary " style="--bs-bg-opacity: .5;">
-      <div class="d-flex justify-content-center mt-3">
-        <img src="<?php echo $row["image"] ?>" class=" w-75 h-auto border border-black" alt="Photo du docteur">
+    <div class="card text-center border border-black m-2 bg-secondary " style="--bs-bg-opacity: .5;">
+      <div class="d-flex justify-content-center m-1 bt-0 pt-0 gt-0">
+        <img src="<?php echo $row["image"] ?>" class=" w-100 h-100 mt-0 bt-0 pt-0 gt-0 border border-black" alt="Photo du docteur">
       </div>
       <div class="card-body">
         <h5 class="card-title">Dr. <?php echo $row["name"] ?></h5><br>
@@ -17,12 +19,12 @@
         <p class="card-text">Description :<br><?php echo $row["description"] ?>
         <figure>
           <p>Échantillon :</p><br>
-          <audio controls src="<?php echo $row["echantillon"] ?>">
+          <audio class="form-control" controls src="<?php echo $row["echantillon"] ?>">
             <a href="<?php echo $row["echantillon"] ?>">Download audio</a>
           </audio>
         </figure>
         <div class="d-flex justify-content-center">
-          <img src="<?php echo $row["signature"] ?>" class=" w-25 h-25 border-bottom-0" alt="Signature du docteur">
+          <img src="<?php echo $row["signature"] ?>" class=" w-25 h-25 border-bottom-0 bg-transparent" alt="Signature du docteur">
         </div>
       </div>
       <div class="card-body">
@@ -30,6 +32,34 @@
         <a href="/medicsong/controller/deleteperso.php?id=<?php echo $row['perso_id'] ?>" class="card-link btn btn-danger border border-black">Supprimer</a>
       </div>
     </div>
+
+    <!-- <div class="row row-cols-1 row-cols-md-3 g-4">
+  <div class="col">
+    <div class="card h-100">
+    <img src="<?php echo $row["image"] ?>" class="card-img-top w-100 h-100 mt-0 bt-0 pt-0 gt-0 border border-black" alt="...">
+      <div class="card-body">
+      <h5 class="card-title text-center fs-6">Dr. <?php echo $row["name"] ?></h5>
+              <p class="fs-6">Spécialité :<br><?php echo $row["spec"] ?></p>
+              <p class="fs-6">Description :<br><?php echo $row["description"] ?>
+              <figure>
+                <p class="fs-6">Échantillon :</p><br>
+                <audio class="form-control" controls src="<?php echo $row["echantillon"] ?>"></audio>
+              </figure>
+                <div class="d-flex justify-content-center">
+                  <img src="<?php echo $row["signature"] ?>" class=" w-25 h-25 border-bottom-0 bg-transparent" alt="Signature du docteur">
+                </div>
+      <div class="card-footer">
+        <small class="text-body-secondary">Last updated 3 mins ago</small>
+      </div>
+    </div>
+    <div class="card-body">
+              <a href="/medicsong/views/formmodifperso.php?id=<?php echo $row['perso_id'] ?>" class="card-link btn btn-success border border-black">Modifier</a>
+              <a href="/medicsong/controller/deleteperso.php?id=<?php echo $row['perso_id'] ?>" class="card-link btn btn-danger border border-black">Supprimer</a>
+            </div> -->
+
+
+
+
 
   <?php }  ?>
 
